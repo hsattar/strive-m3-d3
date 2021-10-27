d4e6f423d8615b5405ccbf819789fd8251d0e4ae
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-27 19:20:50 +0100

    Done Exercise 3

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const hideCards = () => {
+    const hideBtn = document.querySelectorAll('.hideBtn')
+    hideBtn.forEach(btn => btn.addEventListener('click', (e) => {
+    const parentCard = e.target.parentElement.parentElement.parentElement.parentElement
+    parentCard.classList.add('d-none')
+}))
+}
+
+const displaySuccessMessage = (numberOfImages) => {
+    const successAlert = document.querySelector('.alert-success')
+    successAlert.classList.remove('d-none')
+    successAlert.innerText = `${numberOfImages} Images Have Loaded`
+    setTimeout(() => {
+        successAlert.classList.add('d-none')
+    }, 5000)
+}
+
+const displayErrorMessage = (error) => {
+    const errorAlert = document.querySelector('.alert-danger')
+    errorAlert.classList.remove('d-none')
+    errorAlert.innerText = `${error}`
+    setTimeout(() => {
+        errorAlert.classList.add('d-none')
+    }, 5000)
+}
+
+showFirstCarousel = () => {
+    const firstCarouselImage = document.querySelector('.carousel-item')
+    firstCarouselImage.classList.add('active')
+}
 
 const searchData = () => {
     fetch(`https://api.pexels.com/v1/search?query=people`, {
@@ -9,7 +39,7 @@ const searchData = () => {
         const displayImgContainer = document.querySelector('.displayImgContainer')
         displaySuccessMessage(body.photos.length)
         displayImgContainer.innerHTML = 
-        body.photos.map((photo) =>`
+        body.photos.map(photo =>`
         <div class="col-12 col-md-4">
             <div class="card">
                 <img src=${photo.src.small} class="card-img-top pexel-img img-fluid" alt="...">
@@ -37,7 +67,7 @@ const searchData2 = () => {
         const displayImgContainer = document.querySelector('.displayImgContainer')
         displaySuccessMessage(body.photos.length)
         displayImgContainer.innerHTML = 
-        body.photos.map((photo) =>`
+        body.photos.map(photo =>`
         <div class="col-12 col-md-4">
             <div class="card">
                 <img src=${photo.src.small} class="card-img-top pexel-img img-fluid" alt="...">
@@ -65,9 +95,10 @@ const userSearch = () => {
     .then(response => response.json())
     .then(body => {
         const displayImgContainer = document.querySelector('.displayImgContainer')
+        console.log(body.photo)
         displaySuccessMessage(body.photos.length)
         displayImgContainer.innerHTML = 
-        body.photos.map((photo) =>`
+        body.photos.map(photo =>`
         <div class="col-12 col-md-4">
             <div class="card">
                 <img src=${photo.src.small} class="card-img-top pexel-img img-fluid" alt="...">
@@ -86,14 +117,28 @@ const userSearch = () => {
     .catch(error => displayErrorMessage(error))
 }
 
-const hideCards = () => {
-    const hideBtn = document.querySelectorAll('.hideBtn')
-    hideBtn.forEach(btn => btn.addEventListener('click', (e) => {
-    const parentCard = e.target.parentElement.parentElement.parentElement.parentElement
-    parentCard.classList.add('d-none')
-}))
+const carouselImages = () => {
+    fetch(`https://api.pexels.com/v1/search?query=house&per_page=3`, {
+    headers: {
+        Authorization: config.API_KEY
+    }})
+    .then(response => response.json())
+    .then(body => {
+        const carouselContainer = document.querySelector('.carousel-inner')
+        displaySuccessMessage(body.photos.length)
+        carouselContainer.innerHTML = 
+        body.photos.map(photo =>`
+        <div class="carousel-item">
+            <img src=${photo.src.large} class="d-block carousel-image w-100" alt="...">
+        </div>
+        `).join('')
+    })
+    .then(showFirstCarousel)
+    .then(hideCards)
+    .catch(error => displayErrorMessage(error))
 }
 
+carouselImages()
 
 const loadImgBtn = document.querySelector('.loadImgBtn')
 loadImgBtn.addEventListener('click', searchData)
@@ -104,21 +149,3 @@ loadSecondaryBtn.addEventListener('click', searchData2)
 const search = document.querySelector('.searchBtn')
 
 search.addEventListener('click', userSearch)
-
-const displaySuccessMessage = (numberOfImages) => {
-    const successAlert = document.querySelector('.alert-success')
-    successAlert.classList.remove('d-none')
-    successAlert.innerText = `${numberOfImages} Images Have Loaded`
-    setTimeout(() => {
-        successAlert.classList.add('d-none')
-    }, 5000)
-}
-
-const displayErrorMessage = (error) => {
-    const errorAlert = document.querySelector('.alert-danger')
-    errorAlert.classList.remove('d-none')
-    errorAlert.innerText = `${error}`
-    setTimeout(() => {
-        errorAlert.classList.add('d-none')
-    }, 5000)
-}
