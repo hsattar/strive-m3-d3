3025b6627fc57b24538085d0bb6ab5261e263807
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-27 16:05:24 +0200

    task 4 completed.

--- index.js
+++ index.js
@@ -16,15 +16,27 @@ const searchData = () => {
                 <div class="card">
                     <img src=${photo.src.small} class="card-img-top" alt="...">
                     <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button class="hideBtn btn btn-danger">Hide</button>
                     </div>
                 </div>
             </div>
             `
             ).join('')
         })
+    .then(hideCards)
     .catch(error => console.error(error))
 }
+const hideCards = () => {
+    const hideBtn = document.querySelectorAll('.hideBtn')
+console.log(hideBtn)
+hideBtn.forEach(btn => btn.addEventListener('click', (e) => {
+    const parentCard = e.target.parentElement.parentElement.parentElement
+    parentCard.classList.add('d-none')
+}))
+}
+
+
 
 const searchData2 = () => {
     fetch("https://api.pexels.com/v1/search?query=nature", {
@@ -43,7 +55,8 @@ const searchData2 = () => {
                 <div class="card">
                     <img src=${photo.src.small} class="card-img-top pexel-img img-fluid" alt="...">
                     <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button class="btn btn-danger">Hide</button>
                     </div>
                 </div>
             </div>
