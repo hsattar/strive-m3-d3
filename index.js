50d594e49d8479dece4922b2e7f039dda0f35024
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-27 16:39:31 +0200

    Completed tsk five.

--- index.js
+++ index.js
@@ -81,9 +81,55 @@ const hideCards2 = () => {
 }))
 }
 
+const userSearch = () => {
+    const userInput = document.querySelector('.searchInput')
+    fetch(`https://api.pexels.com/v1/search?query=${userInput.value}`, {
+  headers: {
+    Authorization: config.API_KEY
+  }})
+    .then(response => response.json())
+    .then(body => {
+            console.log(body.photos)
+            const displayImgContainer = document.querySelector('.displayImgContainer')
+            displayImgContainer.innerHTML = 
+            
+            body.photos.map((photo) =>
+            `
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <img src=${photo.src.small} class="card-img-top pexel-img img-fluid" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="d-flex justify-content-between">
+                            <button class="hideBtn btn btn-danger">Hide</button>
+                            <p class="text-muted">${photo.id}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            ).join('')
+        })
+        .then(hideCards3)
+        .catch(error => console.error(error))
+    }
+    
+const hideCards3 = () => {
+    const hideBtn = document.querySelectorAll('.hideBtn')
+    hideBtn.forEach(btn => btn.addEventListener('click', (e) => {
+    const parentCard = e.target.parentElement.parentElement.parentElement.parentElement
+    parentCard.classList.add('d-none')
+}))
+}
+
 
 const loadImgBtn = document.querySelector('.loadImgBtn')
 loadImgBtn.addEventListener('click', searchData)
 
 const loadSecondaryBtn = document.querySelector('.loadSecondaryBtn')
 loadSecondaryBtn.addEventListener('click', searchData2)
+
+const search = document.querySelector('.searchBtn')
+
+search.addEventListener('click', userSearch)
+
